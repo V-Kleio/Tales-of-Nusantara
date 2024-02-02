8ebd1d26e573e9587df34cd31e1bf2584c5b5eb3
khalizkr <156478326+khalizkr@users.noreply.github.com>
2024-02-02 23:44:11 +0700

    Add files via upload

---
+++ scripts/Transition.gd
@@ -0,0 +1,13 @@
+extends Control
+
+var transition_duration : float = 1.0
+var fade_color : Color = Color(0, 0, 0, 1)  # Fully opaque black
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
