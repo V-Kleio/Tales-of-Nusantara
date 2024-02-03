873f34402e16eec8736be6a1b6757ea0af7f72b5
KleioV <rafaelmarchel.dw@gmail.com>
2024-02-03 16:14:22 +0700

    Add BGM

--- scripts/dungeon.gd
+++ scripts/dungeon.gd
@@ -1,1 +1,13 @@
 extends Node2D
+
+@onready var dungeon_music = $DungeonMusic
+@onready var boss_1_music = $Boss1Music
+
+func _ready():
+	dungeon_music.play()
+
+
+func _on_boss_music_trigger_body_entered(body):
+	if body.is_in_group('player'):
+		dungeon_music.stop()
+		boss_1_music.play()
